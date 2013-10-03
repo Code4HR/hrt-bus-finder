2e11e053ea39d6035c34b70260c05a210616aa95
Ben Schoenfeld <ben.schoenfeld@gmail.com>
2013-10-03 19:20:38 -0400

    Add stop search feature
    Stop search allows user to drag a map around and find nearby bus stops.
    Clicking on a stop icon will show the user the upcoming bus arrivals for
    that stop. The stop search allows users to set the maps position to
    either their current position or the location of an intersection or
    address.

--- js/busfinder.js
+++ js/busfinder.js
@@ -48,6 +48,10 @@ $(function(){
 		}
 	});
 	
+	var Stop = Backbone.Model.extend({
+		idAttribute: "_id"
+	});
+	
 	var API_URL = 'http://lit-inlet-3610.herokuapp.com/api/'
 	var ArrivalList = Backbone.Collection.extend({ 
 		model: Arrival,
@@ -173,9 +177,30 @@ $(function(){
 			this.markers = [];
 			this.busMarkers = {};
 			this.oldBusMarkers = {};
+			
+			google.maps.event.addListener(this.map, 'dragstart', $.proxy(this.cancelCenterChanged, this));
+			google.maps.event.addListener(this.map, 'dragend', $.proxy(this.readyCenterChanged, this));
+		},
+		
+		setOnCenterChangedEvent: function(onCenterChanged) {
+		    this.onCenterChanged = onCenterChanged;
+		},
+		
+		readyCenterChanged: function() {
+		    this.centerChangedTimeout = setTimeout($.proxy(this.centerChanged, this), 1000);
+		},
+		
+		cancelCenterChanged: function() {
+		    this.centerChangedTimeout && clearTimeout(this.centerChangedTimeout);
+		},
+		
+		centerChanged: function() {
+		    if(this.onCenterChanged) this.onCenterChanged(this.map.getCenter());
 		},
 	    
 	    clear: function() {
+	        this.setOnCenterChangedEvent(null);
+	        
 	        while(this.markers.length) {
 	            this.markers.pop().setMap(null);
             }
@@ -186,21 +211,26 @@ $(function(){
             });
 	    },
 	    
-	    createUserMarker: function(location) {
+	    createUserMarker: function(location, animate) {
 	        var userMarker = new google.maps.Marker({
 				position: location,
+				animation: animate && google.maps.Animation.DROP,
 				map: this.map
 			});
 			this.markers.push(userMarker);
 	    },
 	    
-	    createStopMarker: function(stop) {
+	    createStopMarker: function(stop, animate, onClick) {
 	        var stopPosition = new google.maps.LatLng(stop.get('location')[1], stop.get('location')[0]);
 			var stopMarker = new google.maps.Marker({
 				position: stopPosition,
+				animation: animate && google.maps.Animation.DROP,
 				map: this.map,
 				icon: './img/busstop.png'
 			});
+			
+			onClick && google.maps.event.addListener(stopMarker, 'click', onClick);
+			
 			this.markers.push(stopMarker);
 	    },
 	    
@@ -307,11 +337,19 @@ $(function(){
 	    },
 	    
 	    center: function(location) {
-	        this.map.setCenter(location);
+	        if(location) {
+	            this.map.setCenter(location);
+            } else {
+                return this.map.getCenter();
+            }
 	    },
 	    
 	    resize: function() {
 			google.maps.event.trigger(this.map, 'resize');
+	    },
+	    
+	    zoom: function(zoom) {
+	        this.map.setZoom(zoom);
 	    },
 	    
 	    setDraggable: function(flag) {
@@ -416,23 +454,25 @@ $(function(){
 		}
 	});
 	
+	var LocateUser = function(onLocated) {
+	    var onSuccess = function(position) {
+	        onLocated(new google.maps.LatLng(position.coords.latitude, position.coords.longitude));
+	    };
+	    
+	    var onFail = function() {
+	        onLocated(DowntownNorfolk);
+	    };
+	    
+	    navigator.geolocation ?
+			navigator.geolocation.getCurrentPosition(onSuccess, onFail) : onFail();
+	};
+	
 	var HomeView = Backbone.View.extend({
 	    id: 'stops',
 	    
 		initialize: function() {
-			_.bindAll(this);
-			
-			navigator.geolocation ?
-				navigator.geolocation.getCurrentPosition(this.geolocationSuccess, this.geolocationFail):
-				this.getStopList(DowntownNorfolk);
-		},
-		
-		geolocationSuccess: function(position) {
-			this.getStopList(new google.maps.LatLng(position.coords.latitude, position.coords.longitude));
-		},
-		
-		geolocationFail: function() {
-			this.getStopList(DowntownNorfolk);
+		    _.bindAll(this);
+		    LocateUser(this.getStopList);
 		},
 		
 		getStopList: function(location) {
@@ -524,6 +564,86 @@ $(function(){
 		}
 	});
 	
+	var FindStopsView = Backbone.View.extend({
+	    id: 'find-stops-view',
+	    
+	    template: _.template($('#find-stops-view-template').html()),
+		
+		events: {
+			'click #locate': 'locate',
+			'click #search': 'search'
+		},
+		
+	    initialize: function() {
+	        this.collection = new Backbone.Collection([], {model: Stop});
+	        this.collection.on('add', this.addStop, this);
+	        this.collection.once('sync', function() {App.MapView.setBounds();}, this);
+	        
+	        App.MapView.clear();
+	        App.MapView.setOnCenterChangedEvent($.proxy(this.findClosestStops, this));
+	        
+	        if(this.options.location) {
+	            App.ContentView.once('contentChanged', function() {
+	                this.onUserLocated(this.options.location);
+	            }, this);
+	        } else {
+                this.locate();
+            }
+		},
+		
+		render: function() {
+		    this.$el.html(this.template());
+		    
+		    App.MapView.$el.height(window.innerHeight - $('.navbar').outerHeight(true) - this.$('#find-options').outerHeight(true));
+			this.$('.mapcanvas').html(App.MapView.el);
+			this.$('.mapcanvas').show();
+			
+			return this;
+		},
+		
+		addStop: function(stop) {
+		    App.MapView.createStopMarker(stop, true, function() {
+		        App.Router.navigate('stops/' + stop.get('stopId'), {trigger: true});
+		    });
+		},
+		
+		findClosestStops: function(location) {
+		    App.Router.navigate('findStops/' + location.lat() + '/' + location.lng() + '/');
+		    this.collection.url = API_URL + 'stops/near/' + location.lat() + '/' + location.lng() + '/';
+		    this.collection.fetch({remove: false, dataType: 'jsonp'});
+		},
+		
+		locate: function() {
+		    LocateUser($.proxy(this.onUserLocated, this));
+		},
+		
+		onUserLocated: function(location) {
+		    App.Router.navigate('findStops/' + location.lat() + '/' + location.lng() + '/');
+	        App.MapView.createUserMarker(location, true);
+	        App.MapView.resize();
+	        App.MapView.center(location);
+	        App.MapView.zoom(17);
+	        this.findClosestStops(location);
+	    },
+		
+		search: function() {
+		    var intersection = this.$('#intersection').val();
+		    var city = this.$('#city').val();
+		    if(intersection == '') return;
+		    
+		    App.MapView.clear();
+		    this.collection.once('sync', this.onSearchFinished, this);
+		    this.collection.url = API_URL + 'stops/near/intersection/' + city + '/' + intersection + '/';
+		    this.collection.fetch({dataType: 'jsonp'});
+		},
+		
+		onSearchFinished: function() {
+		    App.MapView.setBounds();
+		    var location = App.MapView.center();
+		    App.Router.navigate('findStops/' + location.lat() + '/' + location.lng() + '/');
+		}
+	});
+	
 	var ContentView = Backbone.View.extend({
 		el: $(".app-container"),
 		
@@ -547,6 +667,7 @@ $(function(){
 			"": "homeView",
 			"stops/*stopIds": "stopView",
 			"routes(/*routeIds)": "routeView",
+			"findStops(/:lat/:lng)(/)": "findStopsView"
 		 },
 		
 		homeView: function() {
@@ -564,6 +685,13 @@ $(function(){
 		routeView: function(routeIds) {
 		    this.clearIntervals();
 		    App.ContentView.setSubView(new RouteView({routeIds: routeIds}));
+		    App.MapView.setDraggable(true);
+		},
+		
+		findStopsView: function(lat, lng) {
+		    this.clearIntervals();
+		    var location = lat && lng && new google.maps.LatLng(lat, lng);
+		    App.ContentView.setSubView(new FindStopsView({location: location}));
 		    App.MapView.setDraggable(true);
 		},
 		
