b84989925dee48a6bb55f08177ddf5b8669a9a6a
Blaine <1wbprice@gmail.com>
2013-10-27 12:09:05 -0400

    order is restored to the grid.

--- js/busfinder.js
+++ js/busfinder.js
@@ -441,7 +441,7 @@ $(function(){
 				var	headerHeight = $('.navbar').height();
 				var	stopHeight = $('.stop-name').height();
 				var	headHeight = $('.head-label').height();
-				var mapHeight = window.innerHeight - (headerHeight + scheduleHeight + stopHeight + headHeight);
+				var mapHeight = window.innerHeight - (headerHeight + scheduleHeight + stopHeight + headHeight + 12);
 				App.MapView.clear();
 				App.MapView.createStopMarker(this.options.stop);
     			App.MapView.createBusMarker(this.model);
