7004745b98575534ce9a01b1a7dfd2633b829491
Stanley Zheng <dev.stanzheng@gmail.com>
2015-03-01 15:05:32 -0500

    adding watcher for browserify, reloading so janky though

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,29 +2,25 @@ module.exports = function(grunt) {
 
     grunt.initConfig({
       pkg: grunt.file.readJSON('package.json'),
-      connect: {
-        server: {
-          options: {
-            keepalive: true,
-            port: 9001,
-            base: './'
-          }
-        }
-      },
       browserify: {
         dist: {
           files: {
             'js/main.js': ['js/modules/*.js'],
           }
         }
-      }
+      },
+      watch: {
+        all: {
+          files: ['js/*.js'],
+          tasks: ['browserify'],
+        },
+      },
     });
 
     // 3. Where we tell Grunt we plan to use this plug-in.
-    grunt.loadNpmTasks('grunt-contrib-connect');
     grunt.loadNpmTasks('grunt-browserify');
-
+    grunt.loadNpmTasks('grunt-contrib-watch');
     // 4. Where we tell Grunt what to do when we type "grunt" into the terminal.
-    grunt.registerTask('default', ['browserify','connect']);
+    grunt.registerTask('default', ['browserify','watch']);
 
 };
