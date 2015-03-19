8dbbd7cae7dacbec9c4d9453b86918261c265ad2
Blaine Price <1wbprice@gmail.com>
2015-03-18 21:49:08 -0400

    Set up connect, browserify.

--- Gruntfile.js
+++ Gruntfile.js
@@ -18,17 +18,44 @@ module.exports = function(grunt) {
             // Gets the port from the connect configuration
             path: 'http://localhost:<%= connect.server.options.port%>'
           }
+        },
+
+        browserify: {
+          dist: {
+            files: {
+              'js/main.js': ['js/collections/*.js', 'js/models/*.js', 'js/views/*.js']
+            }
+          }
+        },
+
+        watch: {
+          scripts: {
+            files: ['js/**/*.js'],
+            tasks: ['browserify'],
+            options: {
+              spawn: false,
+              livereload: true
+            }
+          },
+          styles: {
+            files: ['css/**/*.js'],
+            tasks: ['browserify'],
+            options: {
+              spawn: false,
+              livereload: true
+            }
+          }
         }
 
     });
 
     // 3. Where we tell Grunt we plan to use this plug-in.
     grunt.loadNpmTasks('grunt-contrib-connect');
-    // grunt.loadNpmTasks('grunt-contrib-watch');
-    // grunt.loadNpmTasks('grunt-browserify');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-browserify');
     grunt.loadNpmTasks('grunt-open');
 
     // 4. Where we tell Grunt what to do when we type "grunt" into the terminal.
-    grunt.registerTask('default', ['connect', 'open:dev']);
+    grunt.registerTask('default', ['browserify','connect', 'open:dev', 'watch']);
 
 };
