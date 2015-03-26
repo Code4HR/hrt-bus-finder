85700cc19a565357abb2dd6e02a8cdc046877b22
Blaine Price <1wbprice@gmail.com>
2015-03-26 07:10:16 -0400

    Add gitignore, expose SnowRoutes over moudle exports, remove feedback model.

--- js/views/feedback.js
+++
@@ -1,9 +0,0 @@
-var FeedbackView = Backbone.View.extend({
-		tagName: 'div',
-		template: _.template($('#user-feedback-template').html()),
-		render: function() {
-		    this.$el.html(this.template());
-		    return this;
-		}
-
-	})
