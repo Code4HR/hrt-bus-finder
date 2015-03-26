af5795ea808284ad3b9e4314ca9572a26f29cbbe
Joshua Harris <joshua.jharris13@gmail.com>
2015-03-26 14:24:41 -0400

    Moved parseDate function to utilities/

--- js/utilities/parseDate.js
+++ js/utilities/parseDate.js
@@ -1,5 +1,5 @@
 // parse a date in yyyy-mm-dd format
-function parseDate(input) {
+module.exports = parseDate(input) {
   var parts = input.match(/(\d+)/g);
 	// new Date(year, month [, date [, hours[, minutes[, seconds[, ms]]]]])
 	return new Date(parts[0], parts[1]-1, parts[2]); // months are 0-based
