2492c0112a6e4f1e887f8e0503aca3cc54c60412
Joshua Harris <joshua.jharris13@gmail.com>
2015-03-26 14:15:16 -0400

    Renamed files

--- js/utilities/parseDate.js
+++ js/utilities/parseDate.js
@@ -1,7 +1,7 @@
-	// parse a date in yyyy-mm-dd format
-	function parseDate(input) {
-	  var parts = input.match(/(\d+)/g);
-	  // new Date(year, month [, date [, hours[, minutes[, seconds[, ms]]]]])
-	  return new Date(parts[0], parts[1]-1, parts[2]); // months are 0-based
-	}
+// parse a date in yyyy-mm-dd format
+function parseDate(input) {
+  var parts = input.match(/(\d+)/g);
+	// new Date(year, month [, date [, hours[, minutes[, seconds[, ms]]]]])
+	return new Date(parts[0], parts[1]-1, parts[2]); // months are 0-based
+}
 
