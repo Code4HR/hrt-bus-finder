bc9c2f2177d2cbedbe7048337ccad747e9edf7a7
Blaine Price <1wbprice@gmail.com>
2015-04-02 17:42:58 -0400

    update ActiveRoutesList.js

--- js/collections/ActiveRoutesList.js
+++ js/collections/ActiveRoutesList.js
@@ -1,3 +1,7 @@
+var Backbone = require('backbone'),
+		_ = require('underscore'),
+		$ = require('jquery'),
+		API_URL = require('./../utilities/apiUrl');
 
 	module.exports = Backbone.Collection.extend({
 		url: API_URL + 'routes/active/'
